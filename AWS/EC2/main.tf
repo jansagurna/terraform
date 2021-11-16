85b2508d110bdbab955d3a0d9522e12585e8e61b
Jan Sagurna <58932831+jansagurna@users.noreply.github.com>
2021-11-16 14:36:06 +0100

    update Files

--- AWS/EC2/main.tf
+++ AWS/EC2/main.tf
@@ -1,12 +1,11 @@
 provider "aws" {
   region = "eu-central-1"
-  version = "~> 3.65.0"
 }
 
 resource "aws_instance" "webServer" {
-  ami = "ami-0323c3dd2da7fb37d" // AWS Linux 2
+  ami           = "ami-0323c3dd2da7fb37d" // AWS Linux 2
   instance_type = "t2.micro"
-  key_name = "NEED HERE" // Key Name from EC2 here for authentication
+  key_name      = "NEED HERE" // Key Name from EC2 here for authentication
 
   security_groups = [aws_security_group.allow_ssh.name]
 
@@ -16,23 +15,24 @@ resource "aws_instance" "webServer" {
 }
 
 resource "aws_security_group" "allow_ssh" {
-  name = "security-allow-sh"
+  name        = "security-allow-sh"
   description = "Allow acces to Server via SSH"
 
-    ingress {
-        desdescription = "Allow SSH"
-        from_port = 22
-        to_port = 22
-        protocol = "tcp"
-        cidr_blocks = ["0.0.0.0/0"]
-    }
+  ingress {
+    description = "Allow SSH"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 
-    ingress {
-        from_port   = 80
-        to_port     = 80
-        protocol    = "tcp"
-        cidr_blocks = ["0.0.0.0/0"]
-    }
+  ingress {
+    description = "Allow HTTP"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 }
 
 output "instance_public_dns" {
--- AWS/EC2/readme.md
+++ AWS/EC2/readme.md
@@ -1,7 +1,28 @@
 # Links
-Terraform AWS Provider:
-https://registry.terraform.io/providers/hashicorp/aws/latest
-Terraform AWS Provider Docs:
-https://registry.terraform.io/providers/hashicorp/aws/latest/docs
-Docker Image:
-https://hub.docker.com/r/hashicorp/terraform/tags
+| Tool  | Link |
+| ------------- | ------------- |
+| TF AWS Provider  | https://registry.terraform.io/providers/hashicorp/aws/latest  |
+| TF AWS Provider Docs  | https://registry.terraform.io/providers/hashicorp/aws/latest/docs  |
+| TF Docker Image  | https://hub.docker.com/r/hashicorp/terraform/tags  |
+
+
+
+# Steps
+
+Step 1:
+```
+docker-compose run --rm tf init
+```
+initalsiert den Docker Container \
+\
+Step 2:
+```
+docker-compose run --rm tf fmt
+```
+format all tf files \
+\
+Step 3:
+```
+docker-compose run --rm tf validate
+```
+Check if the File is valid and everything is fine
